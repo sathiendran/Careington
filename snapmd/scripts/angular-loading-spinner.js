13218943b2cbf79a3931a6af4087fdfe38492dea
Prabin Yovan <prabinyovan@gmail.com>
2015-05-12 10:33:13 +0530

    Fixed enabled clicking any icon when loading icon.

--- snapmd/scripts/angular-loading-spinner.js
+++ snapmd/scripts/angular-loading-spinner.js
@@ -14,6 +14,16 @@
                     $rootScope.spinnerActive = loading;
                     if(loading){
                         elm.removeClass('ng-hide');
+                        elm.css({
+                            'top' : 0, 
+                            'left' : 0, 
+                            'right' : 0, 
+                            'bottom' : 0, 
+                            'z-index' : 90000,
+                            'background-color' : '#666',
+                            'opacity' : 0.75,
+                            'position' : 'absolute'
+                        });
                     }else{
                         elm.addClass('ng-hide');
                     }
