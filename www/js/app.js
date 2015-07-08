ca6971e6f4e8de4de72277753eb6afed7b6a0e29
Poomani <poomani@rinsoft.com>
2015-07-08 11:35:28 +0530

    Fixed minor design issue & changed online/offline content.

--- www/js/app.js
+++ www/js/app.js
@@ -19,7 +19,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 		}, 3000);
 		 
 		if (window.cordova && window.cordova.plugins.Keyboard) {
-		  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+		   cordova.plugins.Keyboard.disableScroll(true);
 		}
 		if (window.StatusBar) {
 		  // org.apache.cordova.statusbar required
