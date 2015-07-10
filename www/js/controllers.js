7685ba6db957b99ad09c628baaa5db3778f8a59d
Sathiendran <sathiendran@rinsoft.com>
2015-07-10 18:02:52 +0530

    Html Special character Remove issues Fixed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -171,7 +171,7 @@ angular.module('starter.controllers', ['starter.services','ngLoadingSpinner', 't
         $rootScope.CountrySearchItem = "top: 13px;";
         $rootScope.ConstantTreat = "font-size: 16px;";
 
-        } else if($rootScope.AndroidDevice) {  
+        } else if(!$rootScope.AndroidDevice) {  
         $rootScope.BarHeaderLessDevice = "bar-headerLessAndroid";
         $rootScope.SubHeaderLessDevice = "bar-subheaderLessAndroid";
         $rootScope.HeadTitleLessDevice = "head_titleLessAndroid";
@@ -192,8 +192,6 @@ angular.module('starter.controllers', ['starter.services','ngLoadingSpinner', 't
        
     }
    
-   
-
 	$ionicPlatform.registerBackButtonAction(function (event, $state) {	
         if ( ($rootScope.currState.$current.name=="tab.waitingRoom") ||
 			 ($rootScope.currState.$current.name=="tab.receipt") || 	
@@ -3641,6 +3639,13 @@ $scope.GoTopriorSurgery = function(PriorSurgeryValid) {
         return input;
     };
 }) 
+// Special Charctor Remove Filter //
+.filter('filterHtmlChars', function(){
+  return function(html) {
+    var filtered = angular.element('<div>').html(html).text(); 
+   return filtered;
+  }
+})
 
 .directive('siteHeader', function () {
     return {
