934fca2cafe9faa2a6bdddefd4c272163dea631c
selvapriya <priya.selva3@gmail.com>
2016-03-30 05:43:38 -0700

    add click event

--- www/js/controller/newuserController.js
+++ www/js/controller/newuserController.js
@@ -2,10 +2,16 @@ angular.module('starter.controllers')
 
 .controller('newuserController', function($scope) {
      $scope.addmore = false;
+     
       $scope.moredetails = function() {
         $scope.showme = true;
         $scope.addmore = true;
         $scope.showless= true;
+    };
+      $scope.lessdetails = function() {
+        $scope.showme = false;
+        $scope.addmore = false;
+        $scope.showless= false;
     };
 });
 	
