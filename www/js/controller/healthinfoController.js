a5ddfb71302b9aaf35022603d6b48c9bf824e2a2
selvapriya <priya.selva3@gmail.com>
2016-03-30 05:52:25 -0700

    add hide and show

--- www/js/controller/healthinfoController.js
+++ www/js/controller/healthinfoController.js
@@ -4,6 +4,7 @@ angular.module('starter.controllers')
    $scope.addmore=false;
    $scope.healthhide=true;
    $scope.headerval=false;
+   $scope.editshow=true;
  $scope.profile= function() {
      var myEl = angular.element( document.querySelector( '#profid' ) );
      myEl.addClass('btcolor');
@@ -11,20 +12,20 @@ angular.module('starter.controllers')
   var myEl = angular.element( document.querySelector( '#healid' ) );
       myEl.removeClass('btcolor').css('color','#11c1f3');
       myEl.addClass('btnextcolor');
-  
+       $scope.editshow=true;
        $scope.addmore=false;
        $scope.healthhide=true;    
        $scope.headerval=false;
     }
  $scope.health= function() {
-       $scope.addmore=true;   
+     
      var myEl = angular.element( document.querySelector( '#healid' ) );
          myEl.removeClass('btnextcolor');
          myEl.addClass('btcolor');
      var myEl = angular.element( document.querySelector( '#profid' ) );
          myEl.removeClass('btcolor').css('color','#11c1f3');
          myEl.addClass('btnextcolor');
- 
+  $scope.editshow=false;
      $scope.addmore=true;
        $scope.healthhide=false;  
         $scope.headerval=true;
