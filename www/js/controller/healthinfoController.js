c19ed53adf5a257e0d5a8fdeae414e3965464206
selvapriya <priya.selva3@gmail.com>
2016-03-30 21:52:24 -0700

    add ng-disabled in healthinfo.html

--- www/js/controller/healthinfoController.js
+++ www/js/controller/healthinfoController.js
@@ -5,6 +5,25 @@ angular.module('starter.controllers')
    $scope.healthhide=true;
    $scope.headerval=false;
    $scope.editshow=true;
+   $scope.doneshow=true;
+   $scope.inactive = true;
+   $scope.healthdisable=false;
+   
+   $scope.edittext = function() {
+     $scope.inactive = false;
+      $scope.doneshow=false;
+        $scope.editshow=false;
+        $scope.healthdisable=true;
+   }
+   
+      
+   $scope.donetext = function() {
+      $scope.inactive = true;
+      $scope.doneshow=true;
+       $scope.editshow=true;
+       $scope.healthdisable=false;
+   }
+   
  $scope.profile= function() {
      var myEl = angular.element( document.querySelector( '#profid' ) );
      myEl.addClass('btcolor');
@@ -14,8 +33,9 @@ angular.module('starter.controllers')
       myEl.addClass('btnextcolor');
        $scope.editshow=true;
        $scope.addmore=false;
-       $scope.healthhide=true;    
-       $scope.headerval=false;
+       $scope.healthhide=true;  
+        $scope.doneshow=true;  
+     //  $scope.headerval=false;
     }
  $scope.health= function() {
      
@@ -28,7 +48,8 @@ angular.module('starter.controllers')
   $scope.editshow=false;
      $scope.addmore=true;
        $scope.healthhide=false;  
-        $scope.headerval=true;
+        //$scope.headerval=true;
+         $scope.doneshow=true;
     }
    
 });
