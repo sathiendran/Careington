5f7289f8ce4a171bb1f414ab5fed8b701a685f75
priyajanu <priya.selva3@gmail.com>
2016-07-05 10:40:39 +0100

    popup issues fixed

--- www/js/controller/relateduserController.js
+++ www/js/controller/relateduserController.js
@@ -227,7 +227,7 @@ $rootScope.authorised=relateDependentAuthorize;
             }else{
               var myPopup = $ionicPopup.show({
 
-                  title: "<div class='ion-close-round closeicon' ></div><a class='item-avatar'>  <img src='" + dependentDetails.profileImagePath + "'><span><span class='fname'><b>" + dependentDetails.patientFirstName + "</b></span> <span class='sname'>" + dependentDetails.patientLastName + "</span> <span class='sname'>" + relateDependentAuthorize + "</span> </span></a> ",
+                  title: "<a class='item-avatar'>  <img src='" + dependentDetails.profileImagePath + "'><span><span class='fname'><b>" + dependentDetails.patientFirstName + "</b></span> <span class='sname'>" + dependentDetails.patientLastName + "</span> <span class='sname'>" + relateDependentAuthorize + "</span> </span></a> ",
                   subTitle: "<p class='fontcolor'>" + dependentDetails.gender + $scope.dob + $scope.relationship + "</p>",
                   //   template:'<div class="modal-header"><h3 class="modal-title">Confirm</h3></div><div class="modal-body">{{data.text}}</div><div class="modal-footer"><button class="btn btn-primary" ng-click="ok()">OK</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>',
 
@@ -257,7 +257,9 @@ $rootScope.authorised=relateDependentAuthorize;
                 }
 
             });
-
+            $scope.closepopup=function(){
+                 myPopup.close();
+            }
         };
 
         $scope.userslist = function() {
