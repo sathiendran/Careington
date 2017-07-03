66478ea5452fc203d62e2f8ebe4c62ffa5dab0c6
vetrichelvi <vetrichelvi@rinsoft.com>
2017-07-03 14:35:40 +0530

    registration Agree &create Account button issue fixed

--- www/js/app.js
+++ www/js/app.js
@@ -372,7 +372,7 @@ angular.module('starter', ['ionic', 'ngTouch','starter.controllers', 'starter.se
             //}, 10000);
         }
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
            cordova.plugins.Keyboard.disableScroll(true);
          }
          if(window.StatusBar) {
